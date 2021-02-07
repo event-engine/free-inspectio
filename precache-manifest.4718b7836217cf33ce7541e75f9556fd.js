a539c0ed99424828512efa602614d127a19039f0
codeliner <kontakt@codeliner.ws>
2021-02-07 22:15:18 +0100

    Fix deep links not working

--- precache-manifest.4718b7836217cf33ce7541e75f9556fd.js
+++ precache-manifest.4718b7836217cf33ce7541e75f9556fd.js
@@ -56,12 +56,12 @@ self.__precacheManifest = [
     "url": "https://event-engine.io/free-inspectio/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "https://event-engine.io/free-inspectio/static/media/icons.b87b9ba5.ttf"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "https://event-engine.io/free-inspectio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "https://event-engine.io/free-inspectio/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -88,7 +88,7 @@ self.__precacheManifest = [
     "url": "https://event-engine.io/free-inspectio/static/media/cody.63c5836d.svg"
   },
   {
-    "revision": "7df41f27c698bc606d21e08a164df0c1",
+    "revision": "1710de2e851ab71ea11fa275394852f6",
     "url": "https://event-engine.io/free-inspectio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://event-engine.io/free-inspectio/precache-manifest.02153b9a170e6b5c2e3a9a5e872c4482.js"
+  "https://event-engine.io/free-inspectio/precache-manifest.4718b7836217cf33ce7541e75f9556fd.js"
 );
 
 workbox.clientsClaim();
